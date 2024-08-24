434985c45b7bdcefc75718d45fd1d80c62d08572
Gaurab <khanalgaurab98@gmail.com>
2024-08-24 21:19:17 +0600

    fix build error:

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,24 +1,9 @@
 import MobileSideBar from "@/components/layout/Sidebar/MobileSideBar";
 import SideBar from "@/components/layout/Sidebar/SideBar";
-import { getTrendingMovies } from "@/lib/api/movieAPI/movieAPI";
-import { ITrendingMovies } from "@/types/movie/movieTypes";
-import { useEffect, useState } from "react"
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  const [data, setData] = useState<ITrendingMovies>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getTrendingMovies();
-
-      setData(response.data);
-
-
-      console.log(response);
-    }
-    fetchData();
-  }, [])
+ 
 
   return (
     <div className="bg-black">
