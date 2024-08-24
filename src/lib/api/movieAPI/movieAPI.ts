b1d94fd998b8aebf46998e590067bce2ad19c0cb
Gaurab <khanalgaurab98@gmail.com>
2024-08-24 14:39:38 +0600

    added required apis'

--- src/lib/api/movieAPI/movieAPI.ts
+++ src/lib/api/movieAPI/movieAPI.ts
@@ -5,3 +5,35 @@ export const getTrendingMovies = async () => {
     const response = await axiosInstance.get("/trending/movie/day?language=en-US");
     return response;    
 }
+
+export const getTrendingTvShows = async () => {
+    const response = await axiosInstance.get("/trending/tv/day?language=en-US");
+    return response;    
+}
+
+export const getTopRatedMovies = async (pgNO: number) => {
+    const response = await axiosInstance.get(`/movie/top_rated?language=en-US&page=${pgNO}`);
+    return response;    
+}
+
+export const getTopRatedTvShows = async (pgNO: number) => {
+    const response = await axiosInstance.get(`/tv/top_rated?language=en-US&page=${pgNO}`);
+    return response;    
+}
+
+export const getPopularMovies = async () => {
+    const response = await axiosInstance.get(`/movie/popular`);
+    return response;    
+}
+
+export const getPopularTvShows = async () => {
+    const response = await axiosInstance.get(`/tv/popular`);
+    return response;    
+}
+
+export const getSimilarMovies = async (movieId: number) => {
+    const response = await axiosInstance.get(`/movie/${movieId}/recommendations`);
+    return response;    
+}
+
+
