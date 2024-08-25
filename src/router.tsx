9b9e0b1bdb8687e73cc243ebdfffbb0c6cd1b832
Gaurab <khanalgaurab98@gmail.com>
2024-08-25 10:06:48 +0600

    fix path bug

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Layout from "./pages/layout"
 import Search from "./pages/search/Search"
@@ -7,6 +7,7 @@ const router = createBrowserRouter(
     createRoutesFromElements(
 
         <Route path="/" element={<Layout />}>
+            <Route path="" element={<Navigate to={"/home/movies"}/>}/>
             <Route path="/home/movies" element={<Home />} />
             <Route path="/home/search" element={<Search />} />
             <Route path="/home/favorites/" element={<Home />} />
