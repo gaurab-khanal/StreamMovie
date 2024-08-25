af73784d24017d624e114fe33e65a05645a299d1
Gaurab <khanalgaurab98@gmail.com>
2024-08-25 10:13:31 +0600

    fix

--- src/router.tsx
+++ src/router.tsx
@@ -5,14 +5,15 @@ import Search from "./pages/search/Search"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-
         <Route path="/" element={<Layout />}>
-            <Route path="" element={<Navigate to={"/home/movies"}/>}/>
+            <Route path="/" element={<Navigate to="/home/movies" />} />
+
             <Route path="/home/movies" element={<Home />} />
             <Route path="/home/search" element={<Search />} />
             <Route path="/home/favorites/" element={<Home />} />
             <Route path="/home/top-rated/" element={<Home />} />
         </Route>
+
     )
 )
 
